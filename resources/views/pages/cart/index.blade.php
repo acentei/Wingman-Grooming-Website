76f3320b262e2da0f8709f025584d703a1b3759f
Gillian Tan <tangiza@gmail.com>
2016-12-16 16:46:16 +0800

    Cart Index, mobile fixes and style fixes

--- resources/views/pages/cart/index.blade.php
+++ resources/views/pages/cart/index.blade.php
@@ -6,11 +6,11 @@
 
 {{--META TAGS--}}
 @section('meta-url')
-	{{Request::url()}}
+    {{Request::url()}}
 @endsection
 
 @section('meta-title')
-	Shopping Cart | Wingman Grooming
+    Shopping Cart | Wingman Grooming
 @endsection
 
 @section('meta-description')
@@ -18,7 +18,7 @@
 @endsection
 
 @section('meta-image')
-	
+    
 @endsection
 
 {{-- STYLES AND SCRIPTS--}}
@@ -107,8 +107,8 @@
     
     <div class="voucher-container">
         <label id="v-label" class="control-label" style="display:none;" for="voucher"></label><br>
-        <input id="input-vouch" type="text" name="voucher" placeholder="Enter Voucher Code"/><br>
-        <span id="v-help-b" style="display:none;" class="help-block"></span><br>
+        <input id="input-vouch" type="text" name="voucher" placeholder="Enter Voucher Code"/>
+        <span id="v-help-b" style="display:none;" class="help-block"></span>
         <a id="btnVoucher" href="#">USE VOUCHER</a>
     </div>
 
