512677a6d2582537594b27060196d664dd3bb16c
ACE\Ace <markj.sibayan@gmail.com>
2017-01-30 22:21:11 +0800

    Revert "Footer Changes and Fixes."
    This reverts commit d476912b6b77b13d90982c609ebe42c2ad5c4aa9.

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -4,61 +4,59 @@
             <div class="ft-content">
                 <div class="footer-column">
                     <div class="footer-column-section">
-                        <div class="footer-column-each">
-                            
-                                <label class="lbl-large-bold underline">NEWSLETTER</label>
-                                               
+                        <div class="underline">
+                            <span class="lbl-large-bold">NEWSLETTER</span>
+                        </div>                    
 
-                            <label class="lbl-small">SUBSCRIBE TO OUR MAILING LIST</label>
+                        <label class="lbl-small">SUBSCRIBE TO OUR MAILING LIST</label>
 
-                            {!! Form::open([      
-                                'method' => 'POST',
-                                'url' => 'subscribe'
-                            ]) !!}
+                        {!! Form::open([      
+                            'method' => 'POST',
+                            'url' => 'subscribe'
+                        ]) !!}
 
-                            <div class="foot-subscribe">
-                                <div class="ft-box">
-                                    <input id="txtEmail" name="email" type="email" placeholder="YOUR E-MAIL" required>                                
-                                </div>
-                                <div class="ft-box ft-box-button">
-                                    <input id="btnSubscribe" type="submit" value=" SUBSCRIBE">
-                                </div>
-                                
+                        <div class="foot-subscribe">
+                            <div class="ft-box">
+                                <input id="txtEmail" name="email" type="email" placeholder="YOUR E-MAIL" required>                                
                             </div>
-
-                            {!! Form::close() !!}
+                            <div class="ft-box ft-box-button">
+                                <input id="btnSubscribe" type="submit" value=" SUBSCRIBE">
+                            </div>
+                            
                         </div>
+
+                        {!! Form::close() !!}
                     </div>
                 </div>
 
-                <div class="footer-column">
+                <div class="footer-column ft-column-mid">
                     <div class="footer-column-section">
-                        <div class="footer-column-each ft-column-mid">
-                             <label class="lbl-large-bold underline">NAVIGATION+</label>
-                            <div class="foot-nav">
-                                <ul>
-                                    <li class="footer-column"><a href="{{route('faqs.index')}}">FAQS</a></li>
-                                    <li class="footer-column"><a href="{{route('stockist.index')}}">STOCKISTS</a></li>
-                                    <li class="footer-column"><a href="{{route('policy.index')}}">POLICY</a></li>                    
-                                </ul>
-                            </div> 
-                        </div>                   
+                        <div class="underline">
+                            <span class="lbl-large-bold">NAVIGATION+</span>
+                        </div>
+                        <div class="foot-nav">
+                            <ul>
+                                <li class="footer-column"><a href="{{route('faqs.index')}}">FAQS</a></li>
+                                <li class="footer-column"><a href="{{route('stockist.index')}}">STOCKISTS</a></li>
+                                <li class="footer-column"><a href="{{route('policy.index')}}">POLICY</a></li>                    
+                            </ul>
+                        </div>                    
                     </div>
                 </div>            
 
                 <div class="footer-column ft-column-right">
                     <div class="footer-column-section">
-                        <div class="footer-column-each">
-                            <label class="lbl-large-bold underline">CONNECT+</label>
+                        <div class="underline">
+                            <span class="lbl-large-bold">CONNECT</span>
+                        </div>
 
-                            <label class="lbl-small">GET SOCIAL</label>
-                            <div class="foot-social">
-                                <ul>
-                                    <li class="ft-img-social"><a id="facebook" target="_blank" href="https://www.facebook.com/wingmangrooming" title="Wingman Grooming on Facebook"><img src="{{ URL::asset('/images/facebook.png') }}"></a></li>
-                                    <li class="ft-img-social"><a id="instagram" target="_blank" href="https://www.instagram.com/wingman_grooming" title="Wingman Grooming on Instagram"><img src="{{ URL::asset('/images/instagram.png') }}"></a></li>
-                                    <li class="ft-img-social"><a id="twitter" target="_blank" href="#" title="Wingman Grooming on Twitter"><img src="{{ URL::asset('/images/twitter.png') }}"></a></li>                    
-                                </ul>
-                            </div>
+                        <label class="lbl-small ft-get-social">GET SOCIAL</label>
+                        <div class="foot-social">
+                            <ul>
+                                <li class="ft-img-social"><a id="facebook" target="_blank" href="https://www.facebook.com/wingmangrooming" title="Wingman Grooming on Facebook"><img src="{{ URL::asset('/images/facebook.png') }}"></a></li>
+                                <li class="ft-img-social"><a id="instagram" target="_blank" href="https://www.instagram.com/wingman_grooming" title="Wingman Grooming on Instagram"><img src="{{ URL::asset('/images/instagram.png') }}"></a></li>
+                                <li class="ft-img-social"><a id="twitter" target="_blank" href="#" title="Wingman Grooming on Twitter"><img src="{{ URL::asset('/images/twitter.png') }}"></a></li>                    
+                            </ul>
                         </div>
                     </div>
                 </div>  
